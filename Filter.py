2866ea48c223bbc610563e10e38505a2b26f2fff
GHGDev-11 <67170374+GHGDev-11@users.noreply.github.com>
2020-08-30 14:48:32 +0200

    Add files via upload

--- Filter.py
+++ Filter.py
@@ -21,3 +21,7 @@ class Filter:
     def GetListLength(self, WordList):
         WordListLength = len(WordList)
         return WordListLength
+    
+    def PrintPremadeList(self):
+        WordList = ['arse', 'ass', 'asshole', 'bastard', 'bitch', 'bollocks', 'bugger', 'child-fucker', 'child fucker', 'Christ on a bike', 'Christ on a cracker', 'crap', 'cunt', 'damn', 'effing', 'frigger', 'fuck', 'goddamn', 'hell', 'holy shit', 'horseshit', 'Jesus fuck', 'Jesus wept', 'motherfucker', 'nigga', 'nigger', 'prick', 'shit', 'shit ass', 'shitass', 'slut', 'son of a bitch', 'son of a whore', 'twat', 'piss', 'cum', 'penis', 'dick', 'vagina', 'boobs', 'sex', 'tits', 'boob', 'tit', 'arsehole', 'balls', 'bullshit', 'son of a bitch', 'cock', 'dickhead', 'pussy', 'twat']
+        print(WordList)
